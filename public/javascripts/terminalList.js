7d25fca121065d99a07c1b1da8fe1f55c73114db
liz282907 <422364804@qq.com>
2016-04-26 23:20:44 +0800

    still in bug:add ajax data

--- public/javascripts/terminalList.js
+++ public/javascripts/terminalList.js
@@ -5,16 +5,18 @@ var form = new FormData();
 
 form.append("excel_file",$(".inputfile").get(0).files[0]);
 $("#fileList").on("click",".submit",function(){
+
 		var request = $.ajax({
 			url: '/tel/upload',
 			method:'post',
 			processData:false,
 			contentType:false,
+			data:form,
 			cache:false,
 			success:function(data,textStatus,jqXHR){
 				console.log("-------",data);
 				$(".submit").text("上传成功");
-				console.log("---receive data"+data);
+				console.log("---receive data "+data);
 			},
 			error:function(jqXHR,textStatus,errorThrown){
 				// alert(XMLHttpRequest.status);    //http响应状态
@@ -24,6 +26,7 @@ $("#fileList").on("click",".submit",function(){
 		}).done(function(){
 
 		});
+
 	});
 
 // (function($){
--- routes/tel.js
+++ routes/tel.js
@@ -7,10 +7,13 @@ var upload = multer({
 	dest:path.join(__dirname, 'public','uploads'),
 	fileFilter:function(req,file,cb){
 		var mimetype = file.mimetype.split('/')[1];
+		console.log("filetype ",file.mimetype);
+		/*
 		if(mimetype !=='vnd.ms-excel' || mimetype !=='vnd.openxmlformats-officedocument.spreadsheetml.sheet' ){
 			req.fileValidationError = '请上传excel文件';
 			return cb(null,false,new Error("请上传excel文件"));
 		}
+		*/
 		cb(null,true);
 	}}).single('excel');
 
@@ -19,19 +22,18 @@ router.get('/', function(req, res, next) {
   res.render('telInstitude', { title: '泰尔实验室' });
 });
 router.post('/upload',function(req,res,next){
-	debugger;
 	upload(req,res,function(err){
 		if(err){
 			console.log("multer error ",err);
 			return;
 		}
 		if(req.fileValidationError){
-		res.end(req.fileValidationError);
-		next(new Error(""))
+			res.end(req.fileValidationError);
+			next(new Error(""));
 		}
 		else {
-			console.log("server ",JSON.strinfify(req);
-			res.end(req.file);
+			console.log("server ",req);
+			res.end(req.body);
 		}
 
 	});
